05016c7f858df644c118de531d72bf05e2715ff7
patrick-m17x <pmmcbride@gmail.com>
2019-04-09 21:39:15 -0700

    Add SRGAN-tensorflow notebook and clean up image_resize.py

--- image_resize.py
+++ image_resize.py
@@ -12,33 +12,31 @@ def downscale(name):
         w_new = int(w / scale)
         h_new = int(h / scale)
         im_new = im.resize((w_new, h_new), Image.NEAREST)
-        #save_name_HR = os.path.join(output_dir_HR, "2cstbonniekleiodannyhq_" + name.split('/')[-1].split('.')[0])
         save_name_HR = os.path.join(output_dir_HR, dirname + "_" + name.split('\\')[-1].split('.')[0])
-        #save_name_LR = os.path.join(output_dir_LR, "2cstbonniekleiodannyhq_" + name.split('/')[-1].split('.')[0])
         save_name_LR = os.path.join(output_dir_LR, dirname + "_" + name.split('\\')[-1].split('.')[0])
         im.save(save_name_HR + '.png', format='png')
         im_new.save(save_name_LR + '.png', format='png')
         print("Saved file: {}".format(new_name))
-        
+
 # Define the input and output image directories
-# dirs = ["mfhgbonniebillhq", "mgbfbonnieramonhq", "mshfbonniejohnnyhq", "nafbonniechadhq", "nrgbonniebrucehq"]
-dirs = ["bonnie_test_small"]
+dirs = ["test"]
 for dirname in dirs:
-    input_dir = "./data/bonnie/" + dirname
-    output_dir_HR = './data/bonnie/train_2X_HR'
-    output_dir_LR = './data/bonnie/train_2X_LR'
+    input_dir = "./data/" + dirname
+    output_dir_HR = './data/train_HR'
+    output_dir_LR = './data/train_LR'
     scale = 4
-    
+
     if not os.path.exists(output_dir_HR):
         os.mkdir(output_dir_HR)
-    
+
     if not os.path.exists(output_dir_LR):
         os.mkdir(output_dir_LR)
-    
+
     image_list = os.listdir(input_dir)
-    if os.path.exists(os.path.join(input_dir,'.ipynb_checkpoints')): 
+    if os.path.exists(os.path.join(input_dir,'.ipynb_checkpoints')):
         image_list.remove('.ipynb_checkpoints')
     image_list = [os.path.join(input_dir, _) for _ in image_list]
-    
-    for name in image_list: 
+
+    for name in image_list:
         downscale(name)
+
